76e7d84e89068f9d2034d1fa1b1bdfcee6525c70
tommitah <reijotahvanainen15@gmail.com>
2023-03-23 19:18:00 +0200

    added all controller hooks to router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,5 +3,9 @@ import playerController from '../controllers';
 
 const router = Router();
 router.get('/player', playerController.getAll);
+router.get('/player/:id', playerController.getByAccountId)
+router.post('/player', playerController.create)
+router.put('/player/:id', playerController.update)
+router.delete('/player/:id', playerController.deletePlayer)
 
 export default router;
