934f641e3ccccaa6901e4188c4692735c9ca6fb3
tommitah <reijotahvanainen15@gmail.com>
2023-03-23 19:06:30 +0200

    player controller implemented

--- src/controllers/player.controller.ts
+++ src/controllers/player.controller.ts
@@ -1,14 +1,99 @@
 import { Request, Response, NextFunction } from 'express';
+import Player from '../models/';
 
 const getAll = async (
     _req: Request,
     res: Response,
-    _next: NextFunction
+    next: NextFunction
 ): Promise<void> => {
     // TODO: after model is implemented
-    res.status(200).json({ players: true });
+    try {
+        const players = await Player.findAll({ where: {} });
+        res.json(players);
+    } catch (error) {
+        next(error);
+    }
+};
+
+const getByAccountId = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+): Promise<void> => {
+    try {
+        const { accountId } = req.params;
+        const player = await Player.findOne({
+            where: { account_id: accountId },
+        });
+
+        res.json(player);
+    } catch (error) {
+        next(error);
+    }
+};
+
+const create = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+): Promise<void> => {
+    // const id = uuidv4();
+    try {
+        const newPlayer = await Player.create({ ...req.body /*id*/ });
+        res.json({ newPlayer, msg: 'Player creation successful.' });
+    } catch (error) {
+        next(error);
+    }
+};
+
+// NOTE: still not certain how this will actually be used,
+// this is a preliminary version, changes expected
+const update = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+): Promise<void> => {
+    try {
+        const accountId = req.params;
+        const player = await Player.findOne({
+            where: { account_id: accountId },
+        });
+
+        if (!player)
+            res.json({ msg: `Can't find player for id: ${accountId}` });
+
+        const newPlayer = await player?.update({ ...req.body });
+        res.json(newPlayer);
+    } catch (error) {
+        next(error);
+    }
+};
+
+// NOTE: mostly for use manually
+const deletePlayer = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+): Promise<void> => {
+    try {
+        const accountId = req.params;
+        const player = await Player.findOne({
+            where: { account_id: accountId },
+        });
+
+        if (!player)
+            res.json({ msg: `Can't find player for id: ${accountId}` });
+
+        // NOTE: player? is used since typescript notes that this might be null
+        // something to fix if we end up using this in automation, though I doubt it.
+        const deletedPlayer = await player?.destroy();
+        res.json(deletedPlayer);
+    } catch (error) {}
 };
 
 export default {
     getAll,
+    getByAccountId,
+    create,
+    update,
 };
