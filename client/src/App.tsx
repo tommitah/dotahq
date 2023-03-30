cfd6e01b5e99dec0771c5ef45df5488bd45c4dea
tommitah <reijotahvanainen15@gmail.com>
2023-03-30 18:49:26 +0300

    switched from CRA to Vite, per "official" recommendations

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+    const [count, setCount] = useState(0);
+
     return (
         <div className="App">
-            Hello world!
+            Hello world! {count}
+            <button onClick={() => setCount(count + 1)}>count!</button>
         </div>
     );
 }
